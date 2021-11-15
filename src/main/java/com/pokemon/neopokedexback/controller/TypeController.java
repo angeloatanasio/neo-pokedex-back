a1dbdc60cc43d71368e8ad312542aef1e3518102
angelo <angelo.atanasio93@gmail.com>
2021-11-15 02:21:29 +0100

    suppression import inutile + upgrade version pour dependances + ajout logger dans TypeController

--- src/main/java/com/pokemon/neopokedexback/controller/TypeController.java
+++ src/main/java/com/pokemon/neopokedexback/controller/TypeController.java
@@ -4,6 +4,7 @@ import com.pokemon.neopokedexback.command.NouveauTypeCommand;
 import com.pokemon.neopokedexback.dto.TypeDTO;
 import com.pokemon.neopokedexback.facade.TypeFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/type")
 @RequiredArgsConstructor
+@Slf4j
 public class TypeController {
 
     private final TypeFacade typeFacade;
@@ -20,18 +22,21 @@ public class TypeController {
     @GetMapping(value ="/findAll")
     @CrossOrigin
     public ResponseEntity<List<TypeDTO>> findAll() {
+        log.debug("type controller -> récupérer tous les types  ");
         return ResponseEntity.ok(typeFacade.findAll());
     }
 
     @PostMapping(value = "/save")
     @CrossOrigin
     public ResponseEntity<TypeDTO> enregistrerNouveauType(@RequestBody NouveauTypeCommand nouveauTypeCommand) {
+        log.debug("type controller -> sauvegarde d'un nouveau type ");
         return ResponseEntity.ok(typeFacade.enregistrerNouveautype(nouveauTypeCommand));
     }
 
     @GetMapping(value = "/existe/{data}")
     @CrossOrigin
     public ResponseEntity<Boolean> existeParNom(@PathVariable String data) {
+        log.debug("type controller -> vérifier si un nouveau type existe déjà ");
         return ResponseEntity.ok(typeFacade.existeParNom(data));
     }
 }
--- src/main/java/com/pokemon/neopokedexback/facade/TypeFacade.java
+++ src/main/java/com/pokemon/neopokedexback/facade/TypeFacade.java
@@ -13,7 +13,6 @@ import javax.transaction.Transactional;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Service
